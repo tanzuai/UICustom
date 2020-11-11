f56faf25a6606485143a4798aae82b4a93843d98
谭祖爱 <00274780@yto.net.cn>
2020-11-11 16:38:55 +0800

    添加测试文件

---
+++ library/src/main/java/zuai/tan/library/Test.java
@@ -0,0 +1,12 @@
+package zuai.tan.library;
+
+/**
+ * @ClassName: Test
+ * @Description:
+ * @Author: 谭祖爱
+ */
+public class Test {
+    public void ts(){
+        System.out.println("test  suc");
+    }
+}
